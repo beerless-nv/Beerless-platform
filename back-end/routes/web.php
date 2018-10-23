4d301038c916a3c44183871ed0f521b89236519a
Arno Stoop <stoop.arno@gmail.com>
2018-10-23 15:34:09 +0200

    added simple testroute

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -14,3 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/bierTest', function () use ($router) {
+    return app('db')->select("SELECT * FROM Bier");
+});
