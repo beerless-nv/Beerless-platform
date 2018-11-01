9e8a6cc44aff95ac8d969c20b126c911928faeac
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-11-01 21:34:15 +0100

    added blog and app section to home page

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'Beerless-platform';
+export class AppComponent implements OnInit {
+    title = 'Beerless-platform';
+
+    ngOnInit() {
+    }
 }
