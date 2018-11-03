74235e0aaf14905a7a4d8b7644ad062fd705f7a7
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-11-03 18:09:22 +0100

    added sidebar to bieren

---
+++ front-end/src/app/bieren/bieren-sidebar/bieren-sidebar.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {BierenService} from '../../services/bieren.service';
+
+@Component({
+    selector: 'app-bieren-sidebar',
+    templateUrl: './bieren-sidebar.component.html',
+    styleUrls: []
+})
+export class BierenSidebarComponent implements OnInit {
+
+    @Input() item: { name: string, brewery: string, style: string, image: string };
+    @Input() index: number;
+
+    constructor(private bierenService: BierenService) {
+    }
+
+    ngOnInit() {
+    }
+
+}
--- front-end/src/app/bieren/bieren.module.ts
+++ front-end/src/app/bieren/bieren.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BierenComponent } from './bieren.component';
 import { BierItemComponent } from './bier-item/bier-item.component';
+import { BierenSidebarComponent } from './bieren-sidebar/bieren-sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BierenComponent, BierItemComponent]
+  declarations: [BierenComponent, BierItemComponent, BierenSidebarComponent]
 })
 export class BierenModule { }
