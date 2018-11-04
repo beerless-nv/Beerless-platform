7728ecfa5e45e57030e4b997bc4f6f358d6d82da
Arno Stoop <stoop.arno@gmail.com>
2018-11-04 13:29:45 +0100

    Added insert, delete and update functions for Brouwerijen

--- back-end/app/Http/Controllers/BierController.php
+++ back-end/app/Http/Controllers/BierController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 /**
  * Contains CRUD functions for table 'Bier'.
@@ -12,13 +13,21 @@ class BierController extends BaseController
 {
     /**
      * Returns a specific JSON object of type 'Bier'.
-     * Takes the id as a get parameter.
-     * 
+     * Takes the id as a request parameter.
+     *
+     * @param Request $request
      * @return void
      */
-    public function get() {
-        if(app('request')->id){
-            return json_encode(DB::table('Bier')->where('ID', app('request')->id)->first());
+    public function get(Request $request) {
+        $id = $request->input('id');
+        if($id){
+            $bier = DB::table('Bier')->where('ID', $id)->first();
+            if($bier){
+                return $bier;
+            } else{
+                return "Not Found";
+            }
+            return json_encode();
         } else{
             return "Not Found";
         }        
--- back-end/app/Http/Controllers/BrouwerijController.php
+++ back-end/app/Http/Controllers/BrouwerijController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 /**
  * Contains CRUD functions for table 'Brouwerij'.
@@ -12,13 +13,21 @@ class BrouwerijController extends BaseController
 {
     /**
      * Returns a specific JSON object of type 'Brouwerij'.
-     * Takes the id as a get parameter.
-     * 
+     * Takes the id as a request parameter.
+     *
+     * @param Request $request
      * @return void
      */
-    public function get() {
-        if(app('request')->id){
-            return json_encode(DB::table('Brouwerij')->where('ID', app('request')->id)->first()); 
+    public function get(Request $request) {
+        $id = $request->input('id');
+        if($id){
+            $brouwerij = DB::table('Brouwerij')->where('ID', $id)->first();
+            if($brouwerij){
+                return $brouwerij;
+            } else{
+                return "Not Found";
+            }
+            return json_encode(); 
         } else{
             return "Not Found";
         }
@@ -31,5 +40,95 @@ class BrouwerijController extends BaseController
      */
     public function getAll() { 
         return json_encode(DB::table('Brouwerij')->get());
+    }
+
+    /**
+     * Insert an item into table 'Brouwerij'.
+     * Takes the item fields as request parameters.
+     * Requires the field 'naam'.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function insert(Request $request){
+        $naam = $request->input('naam');
+        if($naam){
+            $brouwerij = array(
+                'naam' => $naam,
+                'beschrijving' => $request->input('beschrijving'),
+                'land' => $request->input('land'),
+                'provincie' => $request->input('provincie'),
+                'plaats' => $request->input('plaats'),
+                'postcode' => $request->input('postcode'),
+                'straatEnNummer' => $request->input('straatEnNummer'),
+                'logo' => $request->input('logo'),
+                'aantalBieren' => $request->input('aantalBieren'),
+                'contactID' => $request->input('contactID')
+            );
+            if(DB::table('Brouwerij')->insertGetId($brouwerij)){
+                return "success";
+            } else{
+                return "fail";
+            }            
+        } else{
+            return "Not Found";
+        }
+        return $request;
+    }
+
+    /**
+     * Deletes an item in table 'Brouwerij'.
+     * Takes the id as a request parameter.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function delete(Request $request){
+        $id = $request->input('id');
+        if($id){
+            $query = DB::table('Brouwerij')->where('ID', $id);
+            if($query->exists()){
+                $query->delete();
+                return "success";
+            } else{
+                return "Not Found";
+            }
+        } else{
+            return "Not Found";
+        }
+    }
+
+    /**
+     * Updates an item in table 'Brouwerij'.
+     * Takes the item fields as request parameters.
+     * Requires the field 'id' and 'naam'.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function update(Request $request){
+        $id = $request->input('id');
+        $naam = $request->input('naam');
+        if($naam && $id){
+            $brouwerij = array(
+                'naam' => $naam,
+                'beschrijving' => $request->input('beschrijving'),
+                'land' => $request->input('land'),
+                'provincie' => $request->input('provincie'),
+                'plaats' => $request->input('plaats'),
+                'postcode' => $request->input('postcode'),
+                'straatEnNummer' => $request->input('straatEnNummer'),
+                'logo' => $request->input('logo'),
+                'aantalBieren' => $request->input('aantalBieren'),
+                'contactID' => $request->input('contactID')
+            );
+            if(DB::table('Brouwerij')->where('ID', $id)->update($brouwerij)){
+                return "success";
+            } else{
+                return "fail";
+            }            
+        } else{
+            return "Not Found";
+        }
     }
 }
