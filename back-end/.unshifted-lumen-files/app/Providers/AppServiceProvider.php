e3010d03ac9954d5052268e07698a14094279b10
Laravel Shift <shift@laravelshift.com>
2018-11-14 15:58:01 +0000

    Shift project folders
    Laravel 5 has a new project folder structure. This commit simply moves
    folders to their new locations in an effort to preserve their Git
    history.

---
+++ back-end/.unshifted-lumen-files/app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+//    public function boot() {
+//        $filesystem = new Filesystem(new Adapter([
+//            'host'     => 'ftp.beerless.be',
+//            'username' => 'beerle1q',
+//            'password' => 'sselreeB1998',
+//
+//            // Optional FTP Settings...
+//            'port'     => 21,
+//            'root'     => '/assets/images/',
+//            // 'passive'  => true,
+//            // 'ssl'      => true,
+//            // 'timeout'  => 30,
+//        ]));
+//    }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment() == 'local') {
+            $this->app->register('Wn\Generators\CommandsServiceProvider');
+        }
+        $this->app->singleton('filesystem', function ($app) {
+            return $app->loadComponent('filesystems', 'Illuminate\Filesystem\FilesystemServiceProvider', 'filesystem');
+        });
+    }
+}
