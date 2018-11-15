0ba185238e6117d1d3a0019b2c3fc28b48b0995a
Arno Stoop <stoop.arno@gmail.com>
2018-11-15 11:48:17 +0100

    Disabled sessions

--- back-end/app/Http/Controllers/GebruikerController.php
+++ back-end/app/Http/Controllers/GebruikerController.php
@@ -27,148 +27,90 @@ class GebruikerController extends Controller
      *
      * @return void
      */
-    // public function getUserData(){
+    public function getUserData(){
 
-    // }
+    }
 
     /**
      * Creates a user based on email, username and password.
      * Takes the email, username and password as request parameter.
      *
-     * @return void
+     * @return Response
      */
-    public function signUp(Request $request)
-    {
-        $retVal;
-        try {
+    public function signUp(Request $request){
+        $retVal;     
+        try{            
+            $retVal = array();
             $email = $requst->input('email');
             $username = $request->input('username');
-            $password = $reuest->input('password');
-            if ($email && $username && $password) {
-                if (!Gebruiker::where('gebruikersnaam', $username)->exists()) {
-                    if (!Gebruiker::where('gebruikersnaam', $username)->exists()) {
+            $password = $reuest->input('password');            
+            if($email && $username && $password){
+                if(!Gebruiker::where('gebruikersnaam', $username)->exists()){
+                    if(!Gebruiker::where('email', $email)->exists()){
+                        $user = new Gebruiker;
+                        $user->gebruikersnaam = $username;
+                        $user->email = $email;
+                        $user->password = $password;
+                        $user->save();
+
+                        $retVal['success'] = true;
+                        $retVal['user'] = $user;
+                    } else{
+                        $retVal['success'] = false;
+                        $retVal['msg'] = 'Email is already in use';
                     }
-                } else {
-                    $retVal->success = false;
-                    $retVal->msg = 'Username already exists';
+                } else{
+                    $retVal['success'] = false;
+                    $retVal['msg'] = 'Username already exists';
                 }
-            } else {
-                $retVal->success = false;
-                $retVal->msg = 'Invalid credentials';
-            }
-        } catch (Exception $e) {
-            $retVal->success = 'error';
-            $retVal->msg = $e->getMessage();
+            } else{
+                $retVal['success'] = false;
+                $retVal['msg'] = 'Invalid credentials';
+            }            
+        } catch (Exception $e){
+            $retVal['success'] = 'error';
+            $retVal['msg'] = $e->getMessage();
         } finally{
             return response()->json($retVal);
         }
-    }
+    }   
 
     /**
      * Checks wether a user has submitted valid credentials on login.
      * Takes the username and password as request parameters.
-     *
+     * 
      * @param Request $request
      * @return Response
      */
-    public function signIn(Request $request)
-    {
-        $retVal;
-        try {
+    public function signIn(Request $request){   
+        $retVal = array();           
+        try{         
             $username = $request->input('username');
-            $password = $reuest->input('password');
-            if ($username && $password) {
-                if (Gebruiker::where('gebruikersnaam', $username)->exists()) {
+            $password = $request->input('password');
+
+            if($username && $password){
+                if(Gebruiker::where('gebruikersnaam', $username)->exists()){
                     $user = Gebruiker::where('gebruikersnaam', $username)->first();
-                    if ($password == $user->wachtwoord) {
-                        $retVal->success = true;
-                        $retVal->user = $user;
-                    } else {
-                        $retVal->success = false;
-                        $retVal->msg = 'Incorrect password';
+                    if($password == $user->wachtwoord){
+                        $retVal['success'] = true;
+                        $retVal['user'] = $user;
+                    } else{
+                        $retVal['success'] = false;
+                        $retVal['msg'] = 'Incorrect password';
                     }
-                } else {
-                    $retVal->success = false;
-                    $retVal->msg = 'User does not exist';
+                } else{
+                    $retVal['success'] = false;
+                    $retVal['msg'] = 'User does not exist';
                 }
-            } else {
-                $retVal->success = false;
-                $retVal->msg = 'Invalid credentials';
-            }
-        } catch (Exception $e) {
-            $retVal->success = 'error';
-            $retVal->msg = $e->getMessage();
+            } else{
+                $retVal['success'] = false;
+                $retVal['msg'] = 'Invalid credentials';
+            }            
+        } catch (Exception $e){
+            $retVal['success'] = 'error';
+            $retVal['msg'] = $e->getMessage();
         } finally{
             return response()->json($retVal);
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public function login(Request $request)
-    {
-        $username = $request->input('username');
-        $password = $request->input('password');
-        $user = $this->checkUserExists($username);
-        if ($user) {
-            if ($this->checkPassword($username, $password)) {
-                return "succesfully logged in";
-            } else {
-                return "Incorrect password";
-            }
-        } else {
-            return "User doesn't exist";
-        }
-    }
-
-    public function checkUserExists(Request $request)
-    {
-        $username = $request->input('username');
-        if (Gebruiker::where('gebruikersnaam', $username)->exists() || Gebruiker::where('email', $username)->exists()) {
-            return Gebruiker::select('id', 'gebruikersnaam')->where('gebruikersnaam', $username)->first();
-        } else {
-            return json_encode(false);
-        }
-    }
-
-    public function checkPassword(Request $request)
-    {
-        $password = $request->input('password');
-        $username = $request->input('username');
-        if ($password == Gebruiker::select("wachtwoord")->where('gebruikersnaam', $username)) {
-            return response()->json(true);
-        } else {
-            return response()->json(false);
-        }
-    }
-
-    public function getUserData(Request $request)
-    {
-        $id = $request->input('id');
-        if ($id) {
-            return response()->json(Gebruiker::select('gebruikersnaam', 'voornaam', 'achternaam', 'profielfoto', 'bio', 'land', 'provincie', 'plaats', 'timestamp')->where('ID', $id)->first());
-        } else {
-            return null;
         }
     }
 }
--- front-end/src/app/pagegroups/Visitor/login/login.component.ts
+++ front-end/src/app/pagegroups/Visitor/login/login.component.ts
@@ -30,19 +30,6 @@ export class LoginComponent implements OnInit {
     login() {
         this.user = this.formLogin.value;
         console.log('wachtwoord: ' + this.user['wachtwoord']);
-        this.loginService.checkUser(this.user['gebruikersnaam']).subscribe((data) => {
-            if(data != false){
-                this.loginService.checkPass(this.user['gebruikersnaam'], this.user['wachtwoord']).subscribe((data2) => {
-                    console.log(data2['wachtwoord'])
-                    if(data2){
-                        console.log('ingelogd');
-                    } else{
-                        console.log('fout wachtwoord');
-                    }
-                })
-            } else{
-                console.log('gebruiker bestaat niet');
-            }
-        });
+        
     }
 }
