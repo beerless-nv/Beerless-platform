85f5d6e3c48c78bf512923840927dedfa002fc0d
Tomnuyts1 <r0661550@student.thomasmore.be>
2018-12-08 22:53:55 +0100

    Added UserSocial model, controller and service backend and social logins frontend

--- front-end/src/app/pagegroups/Visitor/login/login.module.ts
+++ front-end/src/app/pagegroups/Visitor/login/login.module.ts
@@ -4,6 +4,33 @@ import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoginComponent} from './login.component';
 import {NgBootstrapModule} from '../../../sharedModules/ng-bootstrap.module';
+import {
+    AuthServiceConfig,
+    FacebookLoginProvider,
+    GoogleLoginProvider,
+    LinkedinLoginProvider,
+    SocialLoginModule
+} from 'angular-6-social-login';
+
+export function getAuthServiceConfigs() {
+    const config = new AuthServiceConfig(
+        [
+            {
+                id: FacebookLoginProvider.PROVIDER_ID,
+                provider: new FacebookLoginProvider('719680298387454')
+            },
+            {
+                id: GoogleLoginProvider.PROVIDER_ID,
+                provider: new GoogleLoginProvider('648305944502-pbh5nhql3gk1domrop4gimlc2cfnrro0.apps.googleusercontent.com')
+            },
+            // {
+            //     id: LinkedinLoginProvider.PROVIDER_ID,
+            //     provider: new LinkedinLoginProvider('')
+            // },
+        ]
+    );
+    return config;
+}
 
 @NgModule({
     imports: [
@@ -11,9 +38,16 @@ import {NgBootstrapModule} from '../../../sharedModules/ng-bootstrap.module';
         RouterModule,
         FormsModule,
         ReactiveFormsModule,
-        NgBootstrapModule
+        NgBootstrapModule,
+        SocialLoginModule
     ],
-    declarations: [LoginComponent]
+    declarations: [LoginComponent],
+    providers: [
+        {
+            provide: AuthServiceConfig,
+            useFactory: getAuthServiceConfigs
+        }
+    ]
 })
 export class LoginModule {
 }
