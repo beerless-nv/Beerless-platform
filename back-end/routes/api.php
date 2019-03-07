fb0b5ba796b73508a6321c4f698314cfaff58de7
Tomnuyts1 <r0661550@student.thomasmore.be>
2019-03-07 15:55:24 +0100

    cleaned api routes

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -16,78 +16,76 @@ Route::get('/', function () {
 });
 
 Route::prefix('relationships')->group(function() {
-    Route::post('/search', 'RelationshipController@search');
-    Route::get('/', 'RelationshipController@getAll');    
-    Route::post('/', 'RelationshipController@insert');   
+    Route::get('/', 'RelationshipController@getAll');
     Route::get('/{relationshipId}', 'RelationshipController@get');
-    
-    Route::delete('/{relationshipId}', 'RelationshipController@delete');
+    Route::post('/search', 'RelationshipController@search');
+
+    Route::post('/', 'RelationshipController@insert');
     Route::patch('/{relationshipId}', 'RelationshipController@patch');
+    Route::delete('/{relationshipId}', 'RelationshipController@delete');
 });
 
 Route::prefix('statuses')->group(function() {
-    Route::post('/search', 'StatusController@search');
-    Route::get('/', 'StatusController@getAll');    
-    Route::post('/', 'StatusController@insert');   
+    Route::get('/', 'StatusController@getAll');
     Route::get('/{statusId}', 'StatusController@get');
-    
-    Route::delete('/{statusId}', 'StatusController@delete');
+    Route::post('/search', 'StatusController@search');
+
+    Route::post('/', 'StatusController@insert');
     Route::patch('/{statusId}', 'StatusController@patch');
+    Route::delete('/{statusId}', 'StatusController@delete');
 });
 
 Route::prefix('activitytypes')->group(function() {
-    Route::post('/search', 'ActivityTypeController@search');
-    Route::get('/', 'ActivityTypeController@getAll');    
-    Route::post('/', 'ActivityTypeController@insert');   
+    Route::get('/', 'ActivityTypeController@getAll');
     Route::get('/{activitytypeId}', 'ActivityTypeController@get');
-    
-    Route::delete('/{activitytypeId}', 'ActivityTypeController@delete');
+    Route::post('/search', 'ActivityTypeController@search');
+
+    Route::post('/', 'ActivityTypeController@insert');
     Route::patch('/{activitytypeId}', 'ActivityTypeController@patch');
+    Route::delete('/{activitytypeId}', 'ActivityTypeController@delete');
 });
 
 Route::prefix('activities')->group(function() {
-    Route::post('/search', 'ActivityController@search');
-    Route::get('/', 'ActivityController@getAll');    
-    Route::post('/', 'ActivityController@insert');   
+    Route::get('/', 'ActivityController@getAll');
     Route::get('/{activityId}', 'ActivityController@get');
-    
-    Route::delete('/{activityId}', 'ActivityController@delete');
+    Route::post('/search', 'ActivityController@search');
+
+    Route::post('/', 'ActivityController@insert');
     Route::patch('/{activityId}', 'ActivityController@patch');
+    Route::delete('/{activityId}', 'ActivityController@delete');
 });
 
 Route::prefix('articletags')->group(function() {
-    Route::post('/search', 'ArticleTagController@search');
-    Route::get('/', 'ArticleTagController@getAll');    
-    Route::post('/', 'ArticleTagController@insert');   
+    Route::get('/', 'ArticleTagController@getAll');
     Route::get('/{articletagId}', 'ArticleTagController@get');
-    
-    Route::delete('/{articletagId}', 'ArticleTagController@delete');
+    Route::post('/search', 'ArticleTagController@search');
+
+    Route::post('/', 'ArticleTagController@insert');
     Route::patch('/{articletagId}', 'ArticleTagController@patch');
+    Route::delete('/{articletagId}', 'ArticleTagController@delete');
 });
 
 Route::prefix('tags')->group(function() {
-    Route::post('/search', 'TagController@search');
-    Route::get('/', 'TagController@getAll');    
-    Route::post('/', 'TagController@insert');   
+    Route::get('/', 'TagController@getAll');
     Route::get('/{tagId}', 'TagController@get');
-    
-    Route::delete('/{tagId}', 'TagController@delete');
+    Route::post('/search', 'TagController@search');
+
+    Route::post('/', 'TagController@insert');
     Route::patch('/{tagId}', 'TagController@patch');
+    Route::delete('/{tagId}', 'TagController@delete');
 });
 
 Route::prefix('articles')->group(function() {
-    Route::post('/search', 'ArticleController@search');
-    Route::get('/', 'ArticleController@getAll');    
-    Route::post('/', 'ArticleController@insert');   
+    Route::get('/', 'ArticleController@getAll');
     Route::get('/{articleId}', 'ArticleController@get');
-    
-    Route::delete('/{articleId}', 'ArticleController@delete');
+    Route::post('/search', 'ArticleController@search');
+
+    Route::post('/', 'ArticleController@insert');
     Route::patch('/{articleId}', 'ArticleController@patch');
+    Route::delete('/{articleId}', 'ArticleController@delete');
 });
 
 Route::prefix('breweries')->group(function() {
-//    Route::get('/allNameId', 'BreweryController@getAllNameId');
-//    Route::get('/getByName', 'BreweryController@getByName');
     Route::get('/', 'BreweryController@getAll');
     Route::get('/{breweryId}', 'BreweryController@get');
     Route::post('/search', 'BreweryController@search');
@@ -101,40 +99,39 @@ Route::prefix('beers')->group(function() {
     Route::post('/uploadImage', 'BeerController@uploadImage');
     Route::get('/new', 'BeerController@getNewest');
 
-    Route::post('/search', 'BeerController@search');
-    Route::get('/', 'BeerController@getAll');    
-    Route::post('/', 'BeerController@insert');   
+    Route::get('/', 'BeerController@getAll');
     Route::get('/{beerId}', 'BeerController@get');
+    Route::post('/search', 'BeerController@search');
 
-    Route::delete('/{beerId}', 'BeerController@delete');
+    Route::post('/', 'BeerController@insert');
     Route::patch('/{beerId}', 'BeerController@patch');
+    Route::delete('/{beerId}', 'BeerController@delete');
 });
 
 Route::prefix('beertypes')->group(function() {
+    Route::get('/', 'BeertypeController@getAll');
+    Route::get('/{beertypeId}', 'BeertypeController@get');
     Route::post('/search', 'BeertypeController@search');
 
-    Route::get('/', 'BeertypeController@getAll');
     Route::post('/', 'BeertypeController@insert');
-    Route::get('/{beertypeId}', 'BeertypeController@get');
-    
-    Route::delete('/{beertypeId}', 'BeertypeController@delete');
     Route::patch('/{beertypeId}', 'BeertypeController@patch');
+    Route::delete('/{beertypeId}', 'BeertypeController@delete');
 });
 
 Route::prefix('users')->group(function() {
     Route::post('/uploadPicture', 'UserController@uploadPicture');
-
     Route::post('/signUp', 'UserController@signUp');
     Route::post('/signIn', 'UserController@signIn');
-    Route::post('/search', 'UserController@search');
     Route::post('/socialToken', 'UserController@getSocialToken');
 
     Route::get('/', 'UserController@getAll');   
     Route::get('/{userId}', 'UserController@get');
+    Route::post('/search', 'UserController@search');
+
     Route::post('/', 'UserController@insert');
-    Route::delete('/{userId}', 'UserController@delete');
     Route::patch('/patchProfile/{userId}', 'UserController@patchProfile');
     Route::patch('/patchAddress/{userId}', 'UserController@patchAddress');
+    Route::delete('/{userId}', 'UserController@delete');
 });
 
 Route::prefix('usersocials')->group(function() {
@@ -143,4 +140,6 @@ Route::prefix('usersocials')->group(function() {
     Route::post('/search', 'UserSocialController@search');
 
     Route::post('/', 'UserSocialController@insert');
+    Route::patch('/{usersocialId}', 'UserSocialController@patch');
+    Route::delete('/{usersocialId}', 'UserSocialController@delete');
 });
