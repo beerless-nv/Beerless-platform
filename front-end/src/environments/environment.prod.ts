c368757ee8219671149a88dfed0f6cfe09e01c35
Tomnuyts1 <r0661550@student.thomasmore.be>
2019-03-26 10:34:40 +0100

    removed back-end

--- front-end/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: true,
-    backend: 'https://api.beerless.be/public/api/',
-    beerImageURL: 'https://assets.beerless.be/assets/images/beer/image/',
-    beerLogoURL: 'https://assets.beerless.be/assets/images/beer/logo/',
-    breweryLogoURL: 'https://assets.beerless.be/assets/images/brewery/logo/',
-    userPictureURL: 'https://assets.beerless.be/assets/images/user/picture/',
-    articlePictureURL: 'https://assets.beerless.be/assets/images/article/hero-picture/',
-};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    backend: 'https://api.beerless.be/api/',
+    beerImageURL: 'https://assets.beerless.be/assets/images/beer/image/',
+    beerLogoURL: 'https://assets.beerless.be/assets/images/beer/logo/',
+    breweryLogoURL: 'https://assets.beerless.be/assets/images/brewery/logo/',
+    userPictureURL: 'https://assets.beerless.be/assets/images/user/picture/',
+    articlePictureURL: 'https://assets.beerless.be/assets/images/article/hero-picture/',
+};
